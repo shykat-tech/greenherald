6d229623836bbfc9b74c330159521e64f9cb4607
Shykat Raha <shykat@co.design>
2025-12-08 10:58:55 +0600

    fix: files missing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,12 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
 
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon/favicon-96x96.png" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+        { rel: "manifest", href: "/favicon/site.webmanifest" }
       ],  
 
     }
